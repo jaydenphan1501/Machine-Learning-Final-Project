ebd2863880030b0d9de4018a658e5ce4082839db
Max Martin <Maxmcgradymartin@gmail.com>
2026-05-10 20:14:47 -0700

    Added RF

--- Train_model.py
+++ Train_model.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split, TimeSeriesSplit
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 from sklearn.linear_model import LogisticRegression
